50a5773bb098b9567e8d4b049990135f07fb34c0
Sorp <danvuk11@gmail.com>
2020-10-21 15:20:42 +0300

    .

---
+++ Lab 1/C#/Lab_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * В-5
+             * Дано дiйсне число M виду ссс.ddd
+             * Помiняти мiсцями числову та дробову частину мiсцями
+             */
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            Console.WriteLine("Введiть дiйсне число:");
+            
+            float M = float.Parse(Console.ReadLine());
+            
+            int intPart = (int) M;
+            int pointPart = (int) (M % 1 * 1000f);
+            
+            float result = pointPart + intPart * 0.001f;
+            
+            Console.Write("Результат: " + result);
+            
+        }
+    }
+}
